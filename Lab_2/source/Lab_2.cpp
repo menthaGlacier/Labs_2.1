23494e23c7d913e312c2fd278db34f6328b5a5d5
menthaGlacier <dandeliondevelop@gmail.com>
2022-09-14 00:04:31 +0600

    L2 Updated tests

--- Lab_2/source/Lab_2.cpp
+++ Lab_2/source/Lab_2.cpp
@@ -3,42 +3,48 @@
 
 int main()
 {
-	Student student_1;
-	
+	/* Testing constructors */
+        Student student_1;
+	Student student_2 ("Armyboy", 17, 3.2f);
+
 	/* Testing setters */
 	student_1.setName("Carl");
 	student_1.setAge(19);
 	student_1.setGPA(4.7f);
 
-	/* Testing constructors */
-	Student student_2 ("Armyboy", 17, 3.2f);
-
-	/* Testing summing */
-	Student result = student_1 + student_2;
+	/* Testing summing and subtracting */
+	Student result_1 = student_1 + student_2;
+	Student result_2 = student_1 - student_2;
 
 	/* Testing output */
 	std::cout << student_1 << "\n";
 	std::cout << student_2 << "\n";
-	std::cout << result << "\n\n";
+	std::cout << result_1 << "\n";
+	std::cout << result_2 << "\n\n";
 
-	/* Testing operators overloading */
+	/* Testing addition */
 	student_1 = student_1 + "os";
 	student_1 = student_1 + 5;
 	student_1 = student_1 + 0.3f;
 	
+	/* Testing subtraction */
+	/* TODO String subtraction */ 
+	result_1 = result_1 - 7;
+	result_1 = result_1 - 0.2f;
+
+	/* Testing casting */
 	std::cout << "Cast to String: " << static_cast<String>(student_1) << "\n";
 	std::cout << "Cast to int: " << static_cast<int>(student_1) << "\n";
-	std::cout << "Cast to float: " << static_cast<float>(student_1) << "\n";
+	std::cout << "Cast to float: " << static_cast<float>(student_1) << "\n\n";
 
-	student_2 = result;
-	result = result - student_1;
-
-	std::cout << std::endl;
-
-        /* Checking results */
+	/* Testing assignment */
+	student_2 = result_2;
+		
+	/* Checking results */
         std::cout << student_1 << "\n";
 	std::cout << student_2 << "\n";
-	std::cout << result << "\n";
+	std::cout << result_1 << "\n";
+	std::cout << result_2 << "\n\n";
 
 	return 0;
 }
