f6ccdd2796a960194692a29c339d93f788fb5172
Andrew Litvinenko <dandeliondevelop@gmail.com>
2022-09-24 12:38:47 +0700

    L4 Added derived classes

---
+++ Lab_4/include/StudentA.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_A_H
+#define STUDENT_A_H
+
+#include "Student.h"
+
+class StudentA : public Student
+{
+	
+};
+
+#endif /* STUDENT_A_H */
---
+++ Lab_4/include/StudentB.h
@@ -0,0 +1,11 @@
+#ifndef STUDENT_B_H
+#define STUDENT_B_H
+
+#include "Student.h"
+
+class StudentB : public Student
+{
+
+};
+
+#endif /* STUDENT_B_H */
