272d408ece5e161f175840b7bf7396cb7ca8a902
Vortigon <vort.gonov.2016@gmail.com>
2022-09-26 04:00:40 +0700

    L8 queue main functionality added

---
+++ Lab_8/include/Analyzer.h
@@ -0,0 +1,17 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+template<class T>
+class Analyzer
+{
+public:	
+	virtual void generate(size_t amount) = 0;
+	virtual void add(const T& element) = 0;
+	virtual	void clear() = 0;
+	virtual void erase(size_t index) = 0;
+	virtual int find(const T& value) = 0;
+	virtual void sort() = 0;
+	virtual void printData() = 0;
+};
+
+#endif /* ANALYZER_H  */
