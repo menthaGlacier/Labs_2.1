9f8e23631ca4c13f837506db4cd7a9bcb3bf0a11
Andrew Litvinenko <dandeliondevelop@gmail.com>
2022-09-25 23:46:28 +0700

    L2L3 Removed unnecessary overloads

--- Lab_3/source/Lab_3.cpp
+++ Lab_3/source/Lab_3.cpp
@@ -23,16 +23,6 @@ int main()
 	std::cout << result_1 << "\n";
 	std::cout << result_2 << "\n\n";
 
-	/* Testing addition */
-	student_1 = student_1 + "os";
-	student_1 = student_1 + 5;
-	student_1 = student_1 + 0.3f;
-	
-	/* Testing subtraction */
-	result_1 = result_1 - "ok"; 
-	result_1 = result_1 - 7;
-	result_1 = result_1 - 0.2f;
-
 	/* Testing casting */
 	std::cout << "Cast to String: " << static_cast<String>(student_1) << "\n";
 	std::cout << "Cast to int: " << static_cast<int>(student_1) << "\n";
