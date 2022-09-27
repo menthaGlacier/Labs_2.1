0adcfda3389c5c0f72e559445373a9b2a7455082
Vortigon <vort.gonov.2016@gmail.com>
2022-09-27 13:00:02 +0700

    L8 added no trace argument and result table

--- Lab_8/source/Lab_8.cpp
+++ Lab_8/source/Lab_8.cpp
@@ -1,8 +1,22 @@
+bool SHOW_TRACE = true;
+
 #include "../include/QueueAnalyzer.h"
 #include "../include/SetAnalyzer.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc > 1) 
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			if (argv[i][0] == 'n' && argv[i][1] == 't') 
+			{
+			   	SHOW_TRACE = false;
+				break;
+		   	}
+		}
+	}
+
 	QueueAnalyzer<int> q_analyze;
 
 	q_analyze.run(10, 4000);
